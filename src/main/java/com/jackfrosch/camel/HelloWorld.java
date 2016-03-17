d9566e1118461bb92ffe3105b7803eec81e80179
jackfrosch <jfrosch@objistics.com>
2016-03-17 10:19:26 -0500

    HelloWorld class shows basic routing from a polling endpoint (timer) to the log processor

---
+++ src/main/java/com/jackfrosch/camel/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.jackfrosch.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+    final static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
+    public static void main(String[] args) throws Exception {
+
+        CamelContext ctx = new DefaultCamelContext();
+
+        ctx.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("timer:sayHello?period=1s")
+                    .log("Hello");
+            }
+        });
+
+        ctx.start();
+        Thread.sleep(10000);
+        ctx.stop();
+    }
+
+}
