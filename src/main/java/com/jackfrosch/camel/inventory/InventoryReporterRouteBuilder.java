2b8bcb81b9608ab6ca4cf845972ce49590d0719d
jackfrosch <jfrosch@objistics.com>
2016-04-03 22:27:09 -0500

    Added routeIds and Camel Tracer

--- src/main/java/com/jackfrosch/camel/inventory/InventoryReporterRouteBuilder.java
+++ src/main/java/com/jackfrosch/camel/inventory/InventoryReporterRouteBuilder.java
@@ -9,21 +9,26 @@ import org.apache.camel.builder.RouteBuilder;
  * These routes are a bit contrived, but illustrate some fundamental Camel
  * routing and processing ideas
  */
-class InventoryReporterRouteBuilder extends RouteBuilder {
+public class InventoryReporterRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
+        getContext().setTracing(true);
+
         from("file:/tmp/camel-demo/inventory/in?delay=1s&move=../archive") // endpoint is a consumer
+            .routeId("inventoryReporterEntry")
             .convertBodyTo(String.class)
             .process(new InventoryProcessor())
             .to("direct:valueInventory");
 
         from("direct:valueInventory")
+            .routeId("valueInventory")
             .process(new InventoryValuator())
             .to("direct:reportInventory");
 
         from("direct:reportInventory")
+            .routeId("reportInventory")
             .process(new InventoryReportGenerator())
-            .to("file:///tmp/camel-demo/inventory/out");                  //  endpoint is a producer
+            .to("file:/tmp/camel-demo/inventory/out");                    //  endpoint is a producer
 
     }
 }
