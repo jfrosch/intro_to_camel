119e162b174b0ce96fc21b3b98cf3227022c84e1
jackfrosch <jfrosch@objistics.com>
2016-04-05 18:14:57 -0500

    Increased sleep time to 1 min

--- src/main/java/com/jackfrosch/camel/inventory/InventoryReportingApp.java
+++ src/main/java/com/jackfrosch/camel/inventory/InventoryReportingApp.java
@@ -16,7 +16,7 @@ public class InventoryReportingApp {
         ctx.addRoutes(new InventoryReporterRouteBuilder());
 
         ctx.start();
-        Thread.sleep(20000);
+        Thread.sleep(60000);
         ctx.stop();
     }
 }
